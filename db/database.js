447c16785ada52dee05db72bca9f8dc5c1baa0da
Aldwin <aldwin.pag@gmail.com>
2024-04-01 12:29:33 -0400

    implement getTransactions and getCategories databse calls

--- db/database.js
+++ db/database.js
@@ -1,2 +1,29 @@
 const config = require('./knexfile').development;
 const knex = require('knex')(config);
+
+// Queries
+function getTransactions() {
+  return knex('transactions')
+    .join('categories', 'categories.id', 'transactions.category_id')
+    .select(
+      'transactions.id',
+      'date',
+      'name',
+      'is_expense',
+      'amount',
+      'category'
+    )
+    .then((result) => result)
+    .catch((err) => {
+      console.log(err);
+      process.exit(1);
+    });
+}
+
+function getCategories() {
+  return knex('categories').then((result) => console.log(result));
+}
+
+getCategories();
+
+// module.exports = { getTransactions, }
