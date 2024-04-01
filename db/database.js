550a83aaa45973cf7eb240f978b727aa9d75a36f
Aldwin <aldwin.pag@gmail.com>
2024-04-01 12:46:57 -0400

    implement CRUD database queries

--- db/database.js
+++ db/database.js
@@ -2,16 +2,18 @@ const config = require('./knexfile').development;
 const knex = require('knex')(config);
 
 // Queries
+// get all transactions joined with category name
 function getTransactions() {
   return knex('transactions')
     .join('categories', 'categories.id', 'transactions.category_id')
-    .then((result) => console.log(result))
+    .then((result) => result)
     .catch((err) => {
       console.log(err);
       process.exit(1);
     });
 }
 
+// get all categories
 function getCategories() {
   return knex('categories')
     .then((result) => result)
@@ -21,6 +23,18 @@ function getCategories() {
     });
 }
 
+// create category
+function addCategory(category) {
+  return knex('categories')
+    .insert(category, '*')
+    .then((result) => result[0])
+    .catch((err) => {
+      console.log(err);
+      process.exit(1);
+    });
+}
+
+// With given category id, get transactions associated with that category
 function getTransactionsInCategory(category_id) {
   return knex('categories')
     .where('id', category_id)
@@ -30,9 +44,47 @@ function getTransactionsInCategory(category_id) {
         .where('category_id', category.id)
         .then((transactions) => {
           category.transactions = transactions;
-          console.log(category);
+          return category;
+        })
+        .catch((err) => {
+          console.log(err);
+          process.exit(1);
         });
+    })
+    .catch((err) => {
+      console.log(err);
+      process.exit(1);
     });
 }
 
-// module.exports = { getTransactions, getCategories }
+// create transaction
+function addTransaction(transaction) {
+  return knex('transactions')
+    .insert(transaction, '*')
+    .then((result) => result[0])
+    .catch((err) => {
+      console.log(err);
+      process.exit(1);
+    });
+}
+
+// delete transaction
+function deleteTransaction(transaction_id) {
+  return knex('transactions')
+    .del()
+    .where('id', transaction_id)
+    .then((result) => result)
+    .catch((err) => {
+      console.log(err);
+      process.exit(1);
+    });
+}
+
+module.exports = {
+  getTransactions,
+  getCategories,
+  addCategory,
+  getTransactionsInCategory,
+  addTransaction,
+  deleteTransaction,
+};
