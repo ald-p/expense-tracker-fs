0addeb9dd0760798da22fbca36fe78f9b7c67507
Aldwin <aldwin.pag@gmail.com>
2024-04-01 13:32:12 -0400

    setup transactions and categories routes

--- backend/server.js
+++ backend/server.js
@@ -3,9 +3,19 @@ const express = require('express');
 const server = express();
 const PORT = 3000;
 
+// Home Route
 server.get('/', (req, res) => {
   res.json({ message: 'This is the Expense Tracker API' });
 });
+
+/* Router */
+// Transactions Controller
+const transactionsRoute = require('./routes/transactionsRoute');
+server.use('/transactions', transactionsRoute);
+
+// Categories Controller
+const categoriesRoute = require('./routes/categoriesRoute');
+server.use('/categories', categoriesRoute);
 
 server.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
